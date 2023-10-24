53d5f909248a645bbd69510d95303b6af1b4841c
reetikasinghh <139485681+reetikasinghh@users.noreply.github.com>
2023-10-24 22:27:39 +0530

    Add files via upload

---
+++ handling1.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"\nEnter age ";
+    cin>>a;
+    try
+    {
+      if(a>=18)
+    {
+        cout<<"Eligible "<<a;
+    }
+    else
+    {
+throw a;
+    }
+}
+catch(int e)
+{
+    cout<<"\nNot Eligible"<<" error";
+}
+return 0;
+}
+/*Output
+
+Enter age 23
+Eligible 23
+*/
